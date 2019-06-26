59704af4d07e12156600ee5bb35c876a93da4bc8
sgukz <thorapee055@gmail.com>
2019-06-26 09:51:03 +0700

    update 26/06/2562

--- index.js
+++ index.js
@@ -17,33 +17,6 @@ restService.use(
 restService.use(express.static(publicDir));
 restService.use(bodyParser.json());
 
-restService.get('/image/:size', (req, res) => {
-  return new Promise((resolve, reject) => {
-    try {
-      const requestedSize = parseInt(req.params['size']);
-      let fileName = 'public/images/imagemap/richmenu-2500x1686.jpg'
-      let inStream = fs.createReadStream(fileName);
-      let outStream = fs.createWriteStream('public/images/imagemap/' + requestedSize + '.jpg', { flags: "w" });
-      outStream.on('error', function () {
-        console.log("Error");
-      });
-      outStream.on('close', function () {
-        console.log("Successfully saved file");
-        res.type('image/jpeg');
-        fs.createReadStream('public/images/imagemap/' + requestedSize + '.jpg').pipe(res);
-      });
-      let transform = sharp()
-        .resize({ width: requestedSize })
-        .on('info', function (fileInfo) {
-          console.log("Resizing done, file not saved");
-        });
-      resolve(inStream.pipe(transform).pipe(outStream));
-    } catch (error) {
-      reject(error)
-    }
-  })
-});
-
 restService.post("/webhook", function (req, res) {
   const toTwoDigits = num => (num < 10 ? "0" + num : num);
   let today = new Date();
@@ -53,8 +26,9 @@ restService.post("/webhook", function (req, res) {
   let day = toTwoDigits(today.getDate());
   let ToDay = moment().format('LL');
   let date_now = `${year}-${month}-${day}`;
-
-  if (req.body.queryResult.parameters.ot == "เวรบ่าย" || req.body.queryResult.parameters.ot == "เวรบ่ายใคร") {
+  let userMessage = req.body.events[0].message.text;
+  let userId = req.body.events[0].source.userId;
+  if (userMessage == "เวรบ่าย" || userMessage == "เวรบ่ายใคร" || userMessage == "บ่าย") {
     axios
       .post("http://49.231.5.51:3000/getOT", {
         dateStart: date_now
@@ -65,77 +39,71 @@ restService.post("/webhook", function (req, res) {
         let data = resp.data;
         result1 = data.dataParse.name_admin
         result2 = data.dataParse.name_tech
-        return res.json({
-          fulfillmentMessages: [{
-            "payload": {
-              "line": {
-                "type": "flex",
-                "altText": "เวรบ่ายศูนย์คอมพิวเตอร์ ",
-                "contents": {
-                  "type": "bubble",
-                  "styles": {
-                    "header": {
-                      "backgroundColor": "#f39c12"
-                    }
-                  },
-                  "header": {
-                    "type": "box",
-                    "layout": "baseline",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "เวรบ่ายศูนย์คอมพิวเตอร์",
-                        "weight": "bold",
-                        "size": "md",
-                        "gravity": "top",
-                        "color": "#FFFFFF",
-                        "flex": 0
-                      }
-                    ]
-                  },
-                  "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "วันที่ " + ToDay,
-                        "align": "center"
-                      },
-                      {
-                        "type": "text",
-                        "text": "โปรแกรมเมอร์"
-                      },
-                      {
-                        "type": "text",
-                        "text": result1,
-                        "weight": "bold",
-                        "size": "xl",
-                        "align": "center"
-                      },
-                      {
-                        "type": "text",
-                        "text": "ช่างเทคนิค"
-                      },
-                      {
-                        "type": "text",
-                        "text": result2,
-                        "weight": "bold",
-                        "size": "xl",
-                        "align": "center"
-                      }
-                    ]
-                  }
-                }
+        let formatMessage = {
+          "type": "flex",
+          "altText": "เวรบ่ายศูนย์คอมพิวเตอร์ ",
+          "contents": {
+            "type": "bubble",
+            "styles": {
+              "header": {
+                "backgroundColor": "#f39c12"
               }
             },
-            "platform": "LINE"
-          }],
-          source: "line"
-        });
+            "header": {
+              "type": "box",
+              "layout": "baseline",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "เวรบ่ายศูนย์คอมพิวเตอร์",
+                  "weight": "bold",
+                  "size": "md",
+                  "gravity": "top",
+                  "color": "#FFFFFF",
+                  "flex": 0
+                }
+              ]
+            },
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "วันที่ " + ToDay,
+                  "align": "center"
+                },
+                {
+                  "type": "text",
+                  "text": "โปรแกรมเมอร์"
+                },
+                {
+                  "type": "text",
+                  "text": result1,
+                  "weight": "bold",
+                  "size": "xl",
+                  "align": "center"
+                },
+                {
+                  "type": "text",
+                  "text": "ช่างเทคนิค"
+                },
+                {
+                  "type": "text",
+                  "text": result2,
+                  "weight": "bold",
+                  "size": "xl",
+                  "align": "center"
+                }
+              ]
+            }
+          }
+        }
+        reply(userId, formatMessage)
+        res.sendStatus(200)
       })
       .catch(error => console.log("Error :", error));
-  } else if (req.body.queryResult.parameters.duty == "เวรเที่ยง" || req.body.queryResult.parameters.duty == "เวรเที่ยงใคร") {
+  } else if (userMessage == "เวรเที่ยง" || userMessage == "เวรเที่ยงใคร" || userMessage == "เที่ยง") {
     axios
       .post("http://49.231.5.51:3000/getDuty", {
         dateStart: date_now
@@ -144,63 +112,76 @@ restService.post("/webhook", function (req, res) {
         let result = ""
         let data = resp.data;
         result = data.dataParse.name
-        return res.json({
-          fulfillmentMessages: [{
-            "payload": {
-              "line": {
-                "type": "flex",
-                "altText": "เวรเที่ยงศูนย์คอมฯ ",
-                "contents": {
-                  "type": "bubble",
-                  "styles": {
-                    "header": {
-                      "backgroundColor": "#28b463"
-                    }
-                  },
-                  "header": {
-                    "type": "box",
-                    "layout": "baseline",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "เวรเที่ยงศูนย์คอมฯ",
-                        "weight": "bold",
-                        "size": "md",
-                        "gravity": "top",
-                        "color": "#FFFFFF",
-                        "flex": 0
-                      }
-                    ]
-                  },
-                  "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "วันที่ " + ToDay,
-                        "align": "center"
-                      },
-                      {
-                        "type": "text",
-                        "text": result,
-                        "weight": "bold",
-                        "size": "xl",
-                        "align": "center"
-                      }
-                    ]
-                  }
-                }
+        let formatMessage = {
+          "type": "flex",
+          "altText": "เวรเที่ยงศูนย์คอมฯ ",
+          "contents": {
+            "type": "bubble",
+            "styles": {
+              "header": {
+                "backgroundColor": "#28b463"
               }
             },
-            "platform": "LINE"
-          }],
-          source: "line"
-        });
+            "header": {
+              "type": "box",
+              "layout": "baseline",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "เวรเที่ยงศูนย์คอมฯ",
+                  "weight": "bold",
+                  "size": "md",
+                  "gravity": "top",
+                  "color": "#FFFFFF",
+                  "flex": 0
+                }
+              ]
+            },
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "วันที่ " + ToDay,
+                  "align": "center"
+                },
+                {
+                  "type": "text",
+                  "text": result,
+                  "weight": "bold",
+                  "size": "xl",
+                  "align": "center"
+                }
+              ]
+            }
+          }
+        }
+        reply(userId, formatMessage)
+        res.sendStatus(200)
       })
       .catch(error => console.log("Error :", error));
   }
 });
+
+function reply(userId, formatMessage) {
+  let headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer {VdKGNhSVx5cGjQ4bWjbVPmD88+MXVwHCS9oTJu5sZZgx2rFGa+bq93E3oQVo6ExDSZIFApjjR5GT30d+ca7IXPEpS30Ggnvkq1JEuQB2eVsHRJ6cCsZEC1Cf9Em2VPWhvQVxZbSBh6pBMV9HP+f0EQdB04t89/1O/w1cDnyilFU=}'
+  }
+  let body = JSON.stringify({
+    to: userId,
+    messages: [formatMessage]
+  })
+  request.post({
+    url: 'https://api.line.me/v2/bot/message/push',
+    headers: headers,
+    body: body
+  }, (err, res, body) => {
+    console.log('status = ' + res.statusCode);
+  });
+}
+
 
 restService.listen(process.env.PORT || 8000, function () {
   console.log("Server up and listening");
